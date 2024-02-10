4f1c85989a28fd57c8da52882749365fda0cb04d
BluesZero <50782397+BluesZero@users.noreply.github.com>
2024-02-10 11:53:21 -0600

    hangman game
    hangman game in python vanillla

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
 #Hanged Man
 import random
-
+#hangman data
 hangmanSequence = [
     {"drawing": [" ___ ", "|   |", "|   o ", "|", "|"]},
     {"drawing": [" ___ ", "|   |", "|   o ", "|   |", "|"]},
@@ -10,6 +10,7 @@ hangmanSequence = [
     {"drawing": [" ___ ", "|   |", "|   o ", "|  /|\\", "|  / \\"]}
 ]
 
+#Initialize variables
 print("SAVE THE HANGED BROO!!, GUESS THE WOOOORDD!!!")
 wordsList = ['AVOCADO', 'BANANA', 'CHERRY', 'DATE', 'ELDERBERRY', 'FIG', 'GRAPE']
 guessWord = random.choice(wordsList)
@@ -17,19 +18,19 @@ guessedWords = ['_' for _ in range(len(guessWord))]
 lifes = 5
 
 def main():
-  
+  #show remaining letters to guess and draw the hangman   
   while lifes >= 0 and '_' in guessedWords:
     stageDrawing = hangmanSequence[lifes]['drawing']
     print("\n".join(stageDrawing))
     print(' '.join(guessedWords))
     process()
-    
+
+    #if you have no remaining lifes break the bucle
     if lifes < 0 or '_' not in guessedWords:
       break
     
   if lifes < 0:
     print("\nTHE BRO WAS HANGED NOOO000")
-
   else:
     print("\nCONGRATULATIONS! You've saved the brO!")
 
